0c8ab8b2ae142648e615e0dccfc6c10497763aa2
Markus Barenhoff <alios@goran.home.alios.net>
2011-09-27 15:46:02 +0200

    implemented database backend

--- Model.hs
+++ Model.hs
@@ -1,12 +1,148 @@
-{-# LANGUAGE QuasiQuotes, TypeFamilies, GeneralizedNewtypeDeriving, TemplateHaskell, GADTs #-}
+{-# LANGUAGE Rank2Types, TypeSynonymInstances, QuasiQuotes, TypeFamilies, GeneralizedNewtypeDeriving, TemplateHaskell, GADTs #-}
 module Model where
 
 import Yesod
+import Database.Persist.GenericSql
+
+import Data.Enumerator (Iteratee)
+import Data.Maybe (fromJust, isJust, isNothing)
+import qualified Data.Enumerator as E (head)
+import Data.Time.Clock(getCurrentTime, UTCTime(..))
 import Data.Text (Text)
+import Model.Keys
 
+data RoleType = AdminRole | DomainAdminRole | InvSideRole | RefSideRole 
+              deriving (Show, Read, Enum, Eq)                   
+derivePersistField "RoleType"
 
--- You can define all of your database entities in the entities file.
--- You can find more information on persistent and how to declare entities
--- at:
--- http://www.yesodweb.com/book/persistent/
+type ReferenceKey = KeyT LQFBReferenceKey
+derivePersistField "ReferenceKey"
+
+type InviteKey = KeyT LQFBInviteKey
+derivePersistField "InviteKey"
+
 share [mkPersist sqlSettings, mkMigrate "migrateAll"] $(persistFile "config/models")
+
+type Database a = (MonadControlIO m) => SqlPersist m a
+  
+insertKeyset :: DomainId -> Text -> Int -> UserId -> Database KeysetId
+insertKeyset dom name n uid = do
+  t <- liftIO getCurrentTime
+  insert $ Keyset dom name n uid t False False
+  
+activateKeyset :: KeysetId -> Database ()
+activateKeyset set = update set [KeysetActive =. True]
+
+deactivateKeyset :: KeysetId -> Database ()
+deactivateKeyset set = update set [KeysetActive =. False]
+
+keysetStats :: KeysetId -> Database (Maybe (KeysetId, (Int, Int)))
+keysetStats set = do
+  kset' <- get set
+  case (kset') of
+    Nothing -> return Nothing
+    Just kset -> do
+      let s = keysetSize kset
+      ks <- selectList [KeypairKeyset ==. set] []
+      return $ Just (set, (s, length ks))
+  
+selectIncompleteKeysets :: Database [KeysetId]
+selectIncompleteKeysets = do
+  ksets' <- selectList [] []
+  let ksets = map fst ksets'
+  stats <- sequence $ map keysetStats ksets
+  return $ (map $ fst . fromJust) $ filter statsFilter stats
+    where statsFilter Nothing = False
+          statsFilter (Just (_, (s, l))) = l < s
+            
+completeKeyset :: KeysetId -> Database Int
+completeKeyset set = do
+  stats <- keysetStats set
+  case stats of
+    Nothing -> return 0
+    Just (_, (s, l)) -> do
+      k <- getJust set
+      let dom = keysetDomain k
+      ids <- sequence $ replicate (s-l) $ insertRandomKeyPair dom set
+      activateKeyset set
+      return $ length ids
+
+completeKeysets :: Database Int
+completeKeysets = do
+  iks <- selectIncompleteKeysets
+  ns <- sequence $ map completeKeyset iks
+  return $ sum ns
+  
+uniqueRefKey :: DomainId -> Database ReferenceKey
+uniqueRefKey dom = do
+  key <- liftIO randomKey
+  key' <- getBy $ UniqueRefKey dom $ key
+  case key' of
+    Nothing -> return key
+    Just _ -> uniqueRefKey dom
+
+uniqueInvKey :: DomainId -> Database InviteKey
+uniqueInvKey dom = do
+  key <- liftIO randomKey
+  key' <- getBy $ UniqueInvKey dom $ key
+  case key' of
+    Nothing -> return key
+    Just _ -> uniqueInvKey dom
+
+randomUniqueKeyPair :: DomainId -> Database (ReferenceKey, InviteKey)
+randomUniqueKeyPair d = do
+  refKey <- uniqueRefKey d
+  invKey <- uniqueInvKey d
+  return (refKey, invKey)
+  
+insertKeyPair :: DomainId -> 
+                 KeysetId -> 
+                 ReferenceKey -> 
+                 InviteKey -> 
+                 UTCTime -> 
+                 Maybe UTCTime -> 
+                 Maybe UTCTime -> 
+                 Database KeypairId
+
+insertKeyPair dom set refKey invKey creation checkout deaktivated  =
+  insert $ Keypair dom set refKey invKey creation checkout deaktivated 
+
+insertRandomKeyPair :: DomainId -> KeysetId -> Database KeypairId
+insertRandomKeyPair dom set = do
+  (refKey, invKey) <- randomUniqueKeyPair dom
+  t <- liftIO getCurrentTime
+  insertKeyPair dom set refKey invKey t Nothing Nothing
+
+deactivateKey :: ReferenceKey -> Database (Maybe (InviteKey, Bool))
+deactivateKey refKey = do
+  kp' <- selectFirst [KeypairRefKey ==. refKey] []
+  case (kp') of
+    Nothing -> return Nothing
+    Just (kpid, kp) -> do
+      let invKey = keypairInvKey kp
+      let checkout = isJust $ keypairCheckout kp
+      let deactive = isJust $ keypairDeactivated kp
+      if (not deactive) 
+        then do t <- liftIO getCurrentTime
+                update kpid [KeypairDeactivated =. (Just t)]
+        else return ()
+      return $ Just $ (invKey, checkout)
+      
+
+checkoutKey :: DomainId -> ReferenceKey -> Database (Maybe InviteKey)
+checkoutKey dom refKey = do
+  k' <- getBy $ UniqueRefKey dom refKey
+  case k' of
+    Nothing -> return Nothing
+    Just (kid, k) -> do
+      let set' = (keypairKeyset k)
+      set <- getJust set'
+      if (or [isJust $ keypairCheckout k
+             ,isJust $ keypairDeactivated k 
+             ,not $ keysetActive set]) 
+        then return Nothing
+        else do t <- liftIO getCurrentTime
+                update kid [KeypairCheckout =. (Just t)]
+                return $ Just $ keypairInvKey k
+                
+
---
+++ Model/Keys.hs
@@ -0,0 +1,84 @@
+{-# LANGUAGE MultiParamTypeClasses, TypeFamilies, FlexibleContexts #-}
+
+module Model.Keys (KeyC(..), LQFBInviteKey(..), LQFBReferenceKey(..)) where
+
+import Text.Parsec
+import Text.Parsec.Char
+import Text.Parsec.Combinator
+
+import Data.Text (Text)
+import Data.List
+import Data.List.Split (splitEvery)
+import qualified Data.Text as T (pack, unpack, length)
+import Random (Random(..), randomIO)
+
+class KeyC k where
+  data KeyT k :: *
+  parseKey :: Text -> Maybe (KeyT k)
+  keyText :: KeyT k -> Text
+  keyString :: KeyT k -> String
+  randomKey :: IO (KeyT k)
+
+lqfbKeyLength = 20
+lqfbRefKeyTupelLen = 4
+lqfbInvKeyTupelLen = 5
+lqfbTupelSeperator = '-'
+lqfbRefKeyTupels = lqfbKeyLength `div` lqfbRefKeyTupelLen
+lqfbRefKeyTextLen = lqfbRefKeyTupels + lqfbKeyLength - 1
+lqfbInvKeyTupels = lqfbKeyLength `div` lqfbInvKeyTupelLen
+lqfbInvKeyTextLen = lqfbInvKeyTupels + lqfbKeyLength - 1
+
+
+validLQFBKeyChars :: [Char]
+validLQFBKeyChars =
+  filter (\c -> notElem c "aeiou01l") $ ['a'..'z'] ++ ['0' .. '9']
+  
+takeLQFBKeyChars :: Int -> Char
+takeLQFBKeyChars i = validLQFBKeyChars !! (i `mod` (length validLQFBKeyChars))
+  
+
+lqfbTupelSplit n = concat . intersperse [lqfbTupelSeperator] . splitEvery n
+lqfbTupelRandom n  = sequence $ replicate lqfbKeyLength $ fmap n randomIO
+
+lqfbParser tl tc = 
+  let tupelParser = count tl $ oneOf validLQFBKeyChars
+  in do 
+    xs <- count (tc - 1) $ do 
+      t <- tupelParser
+      _ <- char lqfbTupelSeperator
+      return t
+    x <- tupelParser
+    return $ xs ++ [x]
+
+
+data LQFBInviteKey 
+type InviteKey = KeyT LQFBInviteKey
+instance KeyC LQFBInviteKey where
+  data KeyT LQFBInviteKey = LQFBInviteKey [Char]
+                            deriving (Eq, Show, Read)
+  keyText (LQFBInviteKey s) = T.pack $ lqfbTupelSplit lqfbInvKeyTupelLen s
+  keyString (LQFBInviteKey s) = s
+  randomKey = fmap LQFBInviteKey $ lqfbTupelRandom takeLQFBKeyChars
+  parseKey t =
+    let p = lqfbParser lqfbInvKeyTupelLen lqfbInvKeyTupels
+        t' = T.unpack t
+    in case (runParser p () t' t') of
+      Left _ -> Nothing
+      Right ts -> Just $ LQFBInviteKey $ concat ts
+
+
+data LQFBReferenceKey 
+type ReferenceKey = KeyT LQFBReferenceKey
+instance KeyC LQFBReferenceKey where
+  data KeyT LQFBReferenceKey = LQFBReferenceKey [Char]
+                            deriving (Eq, Show, Read)
+  keyText (LQFBReferenceKey s) = T.pack $ lqfbTupelSplit lqfbRefKeyTupelLen s
+  keyString (LQFBReferenceKey s) = s
+  randomKey = fmap LQFBReferenceKey $ lqfbTupelRandom takeLQFBKeyChars
+  parseKey t =
+    let p = lqfbParser lqfbRefKeyTupelLen lqfbRefKeyTupels
+        t' = T.unpack t
+    in case (runParser p () t' t') of
+      Left _ -> Nothing
+      Right ts -> Just $ LQFBReferenceKey $ concat ts
+
